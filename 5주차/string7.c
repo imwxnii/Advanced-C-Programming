b0ddc0fd61a975ae9dddf2cfb9296e05d72669b3
imwxnii <imwxnii@naver.com>
2024-10-16 15:52:01 +0900

    대문자 checking issue 수정

--- 5주차/string7.c
+++ 5주차/string7.c
@@ -4,7 +4,7 @@
 
 int main() {
     char input[200] = "";
-    int count[36] = {0};
+    int count[62] = {0};
     int ch;
     int i = 0;
 
@@ -17,17 +17,17 @@ int main() {
         if(input[i] >= 'a' && input[i] <= 'z') {
             count[input[i] - 'a']++;
         } else if(input[i] >= 'A' && input[i] <= 'Z') {
-            count[input[i] - 'A']++;
+            count[26 + input[i] - 'A']++;
         } else if(input[i] >= '0' && input[i] <= '9') {
-            count[26 + (input[i] - '0')]++;
+            count[52 + (input[i] - '0')]++;
         }
         i++;
     }
 
     printf("입력문자열 : %s\n", input);
     for(int i = 0; i < 26; i++) printf("%c 출현횟수 : %d\n", 'a'+i, count[i]);
-    for(int i = 0; i < 26; i++) printf("%c 출현횟수 : %d\n", 'A'+i, count[i]);
-    for(int i = 0; i < 10; i++) printf("%c 출현횟수 : %d\n", '0'+i, count[26 + i]);
+    for(int i = 0; i < 26; i++) printf("%c 출현횟수 : %d\n", 'A'+i, count[26 + i]);
+    for(int i = 0; i < 10; i++) printf("%c 출현횟수 : %d\n", '0'+i, count[52 + i]);
 
     return 0;
 }
